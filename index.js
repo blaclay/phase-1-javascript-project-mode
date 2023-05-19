2c63960f71d40e04dc5ef7f9f8627bbd68a039fc
Clay Blackwelder <clayblackwelder@gmail.com>
2023-05-18 21:46:30 -0700

    added css for individual articles

--- index.js
+++ index.js
@@ -8,88 +8,34 @@ function displayMain() {
     mainbody.append(p)
 }
 
-function newFunc(isFull) {
-    ///
-    expandButton = createElement('button')
-    if(isFull) {
-        expandButton.addEventListener('click', element => {
-            // loads brief articles
+function newsFull() {
+    section = this.value
+    newsURL = 'https://api.nytimes.com/svc/news/v3/content/nyt/' + section + '.json?api-key=StRnwuvLrlzl1qT0szS7CAKPI2NGEJ65'
+
+    fetch(newsURL)
+        .then(res => res.json())
+        .then(data => {
+            let output = ''
+            console.log(data)
+            data.results.forEach(item => {
+                if (item.multimedia) {
+                    if (item.multimedia[0]) {
+                        output += `<img src="${item.multimedia[0].url}">`
+                    }
+                }
+                output += `<option value="${item.title}">${item.title}</option>`
+                output += `<option value="${item.byline}">${item.byline}</option>`
+                output += `<option value="${item.published_date}">${item.published_date}</option>`
+                output += `<option value="${item.section}">${item.section}</option>`
+                output += `<option value="${item.abstract}">${item.abstract}</option>`
+                output += `<option value="${item.url}">${item.url}</option>`
+            })
+            document.querySelector('#mainbody').innerHTML = output
         })
-    } else {
-        expandButton.addEventListener('click', element)
-    }
-    
 
-    data.result.forEach(item => {
-        articleDiv = createElement('div');
-        articleDiv.id = '$article' +  item.index;
-        if(isFull) {
-            newsFull(item.index, articleDiv);
-        } else {
-            newsBrief(item.index, articleDiv)
-        }
-    })
+        console.log("newsFull()")
 }
 
-function newsFull(index, parent) {
-    buttonBrief = parent.createElement('button')
-    buttonBrief.id = '$showBrief' + index
-    buttonBrief.addEventListener('click', element => {
-
-    })
-}
-
-// function newsBrief() {
-//     section = this.value
-//     newsURL = 'https://api.nytimes.com/svc/news/v3/content/nyt/' + section + '.json?api-key=StRnwuvLrlzl1qT0szS7CAKPI2NGEJ65'
-//     fetch(newsURL)
-//         .then(res => res.json())
-//         .then(data => {
-//             let output = ''
-//             console.log(data)
-//             data.results.forEach(item => {
-//                 if (item.multimedia) {
-//                     if (item.multimedia[0]) {
-//                         output += `<img src="${item.multimedia[0].url}">`
-//                     }
-//                 }
-//                 output += `<option value="${item.title}">${item.title}</option>`
-//                 output += `<option value="${item.byline}">${item.byline}</option>`
-//                 output += `<option value="${item.published_date}">${item.published_date}</option>`
-//                 output += `<option value="${item.section}">${item.section}</option>`
-//                 output += `<option value="${item.abstract}">${item.abstract}</option>`
-//                 output += `<option value="${item.url}">${item.url}</option>`
-//             })
-//             document.querySelector('#mainbody').innerHTML = output
-//         })
-//         console.log("newsBrief()")
-// }
-
-// function newsFull() {
-//     section = this.value
-//     newsURL = 'https://api.nytimes.com/svc/news/v3/content/nyt/' + section + '.json?api-key=StRnwuvLrlzl1qT0szS7CAKPI2NGEJ65'
-//     fetch(newsURL)
-//         .then(res => res.json())
-//         .then(data => {
-//             let output = ''
-//             console.log(data)
-//             data.results.forEach(item => {
-//                 if (item.multimedia) {
-//                     if (item.multimedia[2]) {
-//                         output += `<img src="${item.multimedia[2].url}">`
-//                     }
-//                 }
-//                 output += `<option value="${item.title}">${item.title}</option>`
-//                 output += `<option value="${item.byline}">${item.byline}</option>`
-//                 output += `<option value="${item.published_date}">${item.published_date}</option>`
-//                 output += `<option value="${item.section}">${item.section}</option>`
-//                 output += `<option value="${item.abstract}">${item.abstract}</option>`
-//                 output += `<option value="${item.url}">${item.url}</option>`
-//             })
-//             document.querySelector('#mainbody').innerHTML = output
-//         })
-//         console.log("newsFull()")
-// }
 
 
 fetch(url)
